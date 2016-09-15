f07e392d7eb4c31b1879b7faa35d71273613e117
Andre Luiz da Silva <andreluiz1013@hotmail.com>
2016-09-14 21:53:01 -0300

    JS Hook - Menu-key

---
+++ js/app.js
@@ -0,0 +1,7 @@
+var alternator = document.querySelector('.js-button-key');
+
+alternator.onclick = function(){
+  var menu = document.querySelector('.js-menu');
+
+      menu.classList.toggle('menu-is-show');
+}
